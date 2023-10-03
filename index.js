62da9d68e09a416c936f45e64efca65d5ee50645
shivam sheokand <shivamsheokand32@gmail.com>
2023-10-03 18:53:07 +0530

    get method

--- index.js
+++ index.js
@@ -25,13 +25,27 @@ const app = express();
 app.get('',(req,res)=>{
     // console.log('data sent by Browsers',req.query);
     console.log('data sent by Browsers',req.query.name);
-    res.send('Hello '+req.query.name);
+    res.send(`
+    <h1>Hello ${
+        req.query.name
+    } </h1>
+    <a href='/about'>GO About</a>`);
 });
 app.get('/about',(req,res)=>{
-    res.send('Hello this is About Page');
+    res.send(`
+    <div>
+    <input type="text" Placeholder='Enter your name' />
+    <Button type='submit'>Submit</Button>
+    <a href='/help'>GO Help</a>
+    </div>
+    `);
 });
+const data= require('./json/data.json')
 app.get('/help',(req,res)=>{
-    res.send('Hello this is Help Page');
+    res.send(`
+    <h1>Help</h1>
+    <a href='/'>Go Home</a>
+    `);
 });
 
 app.listen(5000)
